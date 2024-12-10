ceb952fcddca578c23d46188088d68e644750ffe
Ali BENAHMED DAHO <ali.benahmeddaho@transformatek.dz>
2024-12-10 22:05:50 +0000

    Add binarypath to driver

--- experiments-tests-nextjs/test-with-selenium/tests/test-home-page.cjs
+++ experiments-tests-nextjs/test-with-selenium/tests/test-home-page.cjs
@@ -1,46 +1,51 @@
-const { Builder, Browser, By, logging } = require('selenium-webdriver');
-logging.installConsoleHandler();
-logging.getLogger('webdriver.http').setLevel(logging.Level.ALL);
+const { Builder, By } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
 
 (async function testNextJsHomePage() {
-    let driver = await new Builder().forBrowser(Browser.FIREFOX).build()
 
-    // try {
-    // Navigate to the Next.js home page
-    // await driver.get('http://localhost:3000');
+    // set the browser options
+    // const options = new chrome.Options().addArguments('--headless').setBinaryPath("/usr/local/bin/");
+    const options = new chrome.Options().setBinaryPath("/usr/local/bin");
 
-    // // Verify the page title (h1)
-    // let titleElement = await driver.findElement(By.tagName('h1'));
-    // let titleText = await titleElement.getText();
-    // console.log('Title Text:', titleText);
+    // initialize the webdriver
+    const driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
-    // if (titleText === 'Welcome to My Next.js App') {
-    //     console.log('Title Test Passed!');
-    // } else {
-    //     console.error('Title Test Failed!');
-    // }
+    try {
+        // Navigate to the Next.js home page
+        await driver.get('http://localhost:3000');
 
-    // // Verify the paragraph text
-    // let paragraphElement = await driver.findElement(By.tagName('p'));
-    // let paragraphText = await paragraphElement.getText();
-    // console.log('Paragraph Text:', paragraphText);
+        // Verify the page title (h1)
+        let titleElement = await driver.findElement(By.tagName('h1'));
+        let titleText = await titleElement.getText();
+        console.log('Title Text:', titleText);
 
-    // if (paragraphText.includes('This is a simple homepage')) {
-    //     console.log('Paragraph Test Passed!');
-    // } else {
-    //     console.error('Paragraph Test Failed!');
-    // }
+        if (titleText === 'Welcome to My Next.js App') {
+            console.log('Title Test Passed!');
+        } else {
+            console.error('Title Test Failed!');
+        }
 
-    // // Click the button
-    // let buttonElement = await driver.findElement(By.tagName('button'));
-    // await buttonElement.click();
+        // Verify the paragraph text
+        let paragraphElement = await driver.findElement(By.tagName('p'));
+        let paragraphText = await paragraphElement.getText();
+        console.log('Paragraph Text:', paragraphText);
 
-    // console.log('Button Clicked!');
-    // } catch (error) {
-    //     console.error('Test Error:', error);
-    // } finally {
-    //     // Close the browser
-    //     await driver.quit();
-    // }
+        if (paragraphText.includes('This is a simple homepage')) {
+            console.log('Paragraph Test Passed!');
+        } else {
+            console.error('Paragraph Test Failed!');
+        }
+
+        // Click the button
+        let buttonElement = await driver.findElement(By.tagName('button'));
+        await buttonElement.click();
+
+        console.log('Button Clicked!');
+    } catch (error) {
+        console.error('Test Error:', error);
+    } finally {
+        // Close the browser
+        await driver.quit();
+    }
 })();
