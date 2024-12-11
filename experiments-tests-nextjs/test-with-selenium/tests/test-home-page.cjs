34a3ad220b285f36812dd3f9d3a12f18d8b491ad
ziadi kheira <kheira.ziadi@transformatek.dz>
2024-12-11 10:05:50 +0000

    Update readme

--- experiments-tests-nextjs/test-with-selenium/tests/test-home-page.cjs
+++ experiments-tests-nextjs/test-with-selenium/tests/test-home-page.cjs
@@ -5,47 +5,47 @@ const chrome = require('selenium-webdriver/chrome');
 (async function testNextJsHomePage() {
 
     // set the browser options
-    // const options = new chrome.Options().addArguments('--headless').setBinaryPath("/usr/local/bin/");
-    const options = new chrome.Options().setBinaryPath("/usr/local/bin");
+    const options = new chrome.Options().addArguments('--headless').setBinaryPath("/usr/local/bin/chromedriver");
+    // const options = new chrome.Options().addArguments('--headless');
 
     // initialize the webdriver
     const driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
-    try {
-        // Navigate to the Next.js home page
-        await driver.get('http://localhost:3000');
+    // try {
+    //     // Navigate to the Next.js home page
+    //     await driver.get('http://localhost:3000');
 
-        // Verify the page title (h1)
-        let titleElement = await driver.findElement(By.tagName('h1'));
-        let titleText = await titleElement.getText();
-        console.log('Title Text:', titleText);
+    //     // Verify the page title (h1)
+    //     let titleElement = await driver.findElement(By.tagName('h1'));
+    //     let titleText = await titleElement.getText();
+    //     console.log('Title Text:', titleText);
 
-        if (titleText === 'Welcome to My Next.js App') {
-            console.log('Title Test Passed!');
-        } else {
-            console.error('Title Test Failed!');
-        }
+    //     if (titleText === 'Welcome to My Next.js App') {
+    //         console.log('Title Test Passed!');
+    //     } else {
+    //         console.error('Title Test Failed!');
+    //     }
 
-        // Verify the paragraph text
-        let paragraphElement = await driver.findElement(By.tagName('p'));
-        let paragraphText = await paragraphElement.getText();
-        console.log('Paragraph Text:', paragraphText);
+    //     // Verify the paragraph text
+    //     let paragraphElement = await driver.findElement(By.tagName('p'));
+    //     let paragraphText = await paragraphElement.getText();
+    //     console.log('Paragraph Text:', paragraphText);
 
-        if (paragraphText.includes('This is a simple homepage')) {
-            console.log('Paragraph Test Passed!');
-        } else {
-            console.error('Paragraph Test Failed!');
-        }
+    //     if (paragraphText.includes('This is a simple homepage')) {
+    //         console.log('Paragraph Test Passed!');
+    //     } else {
+    //         console.error('Paragraph Test Failed!');
+    //     }
 
-        // Click the button
-        let buttonElement = await driver.findElement(By.tagName('button'));
-        await buttonElement.click();
+    //     // Click the button
+    //     let buttonElement = await driver.findElement(By.tagName('button'));
+    //     await buttonElement.click();
 
-        console.log('Button Clicked!');
-    } catch (error) {
-        console.error('Test Error:', error);
-    } finally {
-        // Close the browser
-        await driver.quit();
-    }
+    //     console.log('Button Clicked!');
+    // } catch (error) {
+    //     console.error('Test Error:', error);
+    // } finally {
+    //     // Close the browser
+    //     await driver.quit();
+    // }
 })();
